e4f72f59471df1aae2bee9742b8d4f6ee7513c3a
thrashedbrain <yakura12kk@gmail.com>
2023-02-03 08:52:04 +0700

    Add timer to close yolov5 in idle

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import io
-
+import gc
 import flask
 import torch
+import threading
 from PIL import Image
 from flask import Flask, request, send_file
 
@@ -9,14 +10,39 @@ app = Flask(__name__)
 
 DETECTION_ENDPOINT = "/v1/detect"
 INIT_ENDPOINT = "/v1/init"
+TIMER_SECS = 5 * 60.0
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
+model = None
+
+timer: threading.Timer = None
+
+
+def timerTask():
+    global model
+    del model
+    gc.collect()
+    print("timer end")
 
 
 @app.route(INIT_ENDPOINT, methods=["POST"])
 def init():
-    global model
+    global model, timer
     model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
+    if timer is None:
+        timer = threading.Timer(TIMER_SECS, timerTask)
+        timer.start()
+        print("timer none")
+    else:
+        if timer.is_alive():
+            timer.cancel()
+            timer = threading.Timer(TIMER_SECS, timerTask)
+            timer.start()
+            print("Timer restarted")
+
+        else:
+            timer = threading.Timer(TIMER_SECS, timerTask)
+            timer.start()
+            print("Timer started")
     return flask.jsonify(status="ok")
 
 
@@ -31,30 +57,33 @@ def detect():
     if not request.method == "POST":
         return
 
-    image_file = request.files["image"]
-    image_bytes = image_file.read()
-    img = Image.open(io.BytesIO(image_bytes))
-    result = model(img, 640)
-    usage_counter = usage_counter + 1
-    img_io = io.BytesIO()
-    data = result.pandas().xyxy[0]
-    line = data.iloc[[0]]
+    try:
+        image_file = request.files["image"]
+        image_bytes = image_file.read()
+        img = Image.open(io.BytesIO(image_bytes))
+        result = model(img, 640)
+        usage_counter = usage_counter + 1
+        img_io = io.BytesIO()
+        data = result.pandas().xyxy[0]
+        line = data.iloc[[0]]
 
-    xmin = line['xmin'][0]
-    xmax = line['xmax'][0]
+        xmin = line['xmin'][0]
+        xmax = line['xmax'][0]
 
-    ymin = line['ymin'][0]
-    ymax = line['ymax'][0]
+        ymin = line['ymin'][0]
+        ymax = line['ymax'][0]
 
-    area = (xmin, ymin, xmax, ymax)
-    croped = img.crop(area)
-    croped.save(img_io, format="JPEG")
-    img_io.seek(0)
-    return send_file(
-        img_io,
-        download_name="result",
-        mimetype='image/jpeg'
-    )
+        area = (xmin, ymin, xmax, ymax)
+        croped = img.crop(area)
+        croped.save(img_io, format="JPEG")
+        img_io.seek(0)
+        return send_file(
+            img_io,
+            download_name="result",
+            mimetype='image/jpeg'
+        )
+    except:
+        return "Err"
 
 
 if __name__ == '__main__':
